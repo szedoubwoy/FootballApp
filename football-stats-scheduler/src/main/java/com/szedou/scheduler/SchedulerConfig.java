4f132b32d7b758b257e65a9591218470b1659ca8
Szedou <szedouuu@gmail.com>
2025-02-11 22:17:53 +0100

    move migrations

--- football-stats-scheduler/src/main/java/com/szedou/scheduler/SchedulerConfig.java
+++
@@ -1,9 +0,0 @@
-package com.szedou.scheduler;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = {"com.szedou.api", "com.szedou.scheduler"})
-public class SchedulerConfig {
-}
